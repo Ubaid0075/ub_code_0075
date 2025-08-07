9ffef7654d417b8e0b72824fba255a5c7fceba78
Ubaid <mdub0075@gmail.com>
2025-08-07 12:04:57 +0530

    test commit

--- MeterToFeet.java
+++ MeterToFeet.java
@@ -4,7 +4,7 @@ public class MeterToFeet {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter distance in meters: ");
+        System.out.print("Enter distance in d meters: ");
         double meters = input.nextDouble();
 
         double feet = meters * 3.28084;
