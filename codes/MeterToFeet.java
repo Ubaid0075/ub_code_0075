b28ad2f1072e0e29884bb6662e11c6787a5267a8
Ubaid <mdub0075@gmail.com>
2025-08-21 14:33:07 +0530

    changes

--- codes/MeterToFeet.java
+++ codes/MeterToFeet.java
@@ -1,11 +1,10 @@
-package codes;
 import java.util.Scanner;
 
 public class MeterToFeet {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter distance in d meters: ");
+        System.out.print("Enter distance in meters: ");
         double meters = input.nextDouble();
 
         double feet = meters * 3.28084;
