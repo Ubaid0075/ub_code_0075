6d215ef08e206a15436799e8cd2115135823ad5c
Ubaid <mdub0075@gmail.com>
2025-09-21 10:18:38 +0530

    add initial implementation of Calculator class

--- codes/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-public class AddressSingle {
-    public static void main(String[] args) {
-         System.out.println("My name is Ubaid, ");
-        System.out.println("I live in Noida, Uttar Pradesh, India.");
-        System.out.print("My name is Ubaid, I live in Noida, Uttar Pradesh, India.");
-       
-    }
-}
